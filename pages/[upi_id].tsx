600aa56ec2edda20554452e8a0b0798bfe32d3aa
Amal Shaji <amalshajid@gmail.com>
2022-03-01 19:07:43 +0530

    Update SEO metadata

--- pages/[upi_id].tsx
+++ pages/[upi_id].tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import UpayLinkCard from '../components/UpayLinkCard'
+import Head from 'next/head'
 
 // @ts-ignore
 export async function getServerSideProps(context) {
@@ -11,12 +12,53 @@ export async function getServerSideProps(context) {
 }
 // @ts-ignore
 const UPIPage: NextPage = ({ upi_data }) => {
+  const { upi_id, amount } = upi_data
   return (
-    <div className="h-screen w-full content-center overflow-y-clip bg-gradient-to-br from-teal-600 to-blue-600">
-      <div className="py-14 px-2 sm:py-36">
-        <UpayLinkCard upi_data={upi_data} />
+    <>
+      <Head>
+        <title>UPay</title>
+        <meta name="title" content="UPay" />
+        <meta
+          name="description"
+          content={
+            amount
+              ? `${upi_id} is requesting ₹${amount}`
+              : `Generate shareable UPI payment links`
+          }
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://upay.cf/" />
+        <meta property="og:title" content="UPay" />
+        <meta
+          property="og:description"
+          content={
+            amount
+              ? `${upi_id} is requesting ₹${amount}`
+              : `Generate shareable UPI payment links`
+          }
+        />
+        <meta property="og:image" content="https://upay.cf/card.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://upay.cf/" />
+        <meta property="twitter:title" content="UPay" />
+        <meta
+          property="twitter:description"
+          content={
+            amount
+              ? `${upi_id} is requesting ₹${amount}`
+              : `Generate shareable UPI payment links`
+          }
+        />
+        <meta property="twitter:image" content="https://upay.cf/card.png" />
+      </Head>
+      <div className="h-screen w-full content-center overflow-y-clip bg-gradient-to-br from-teal-600 to-blue-600">
+        <div className="py-14 px-2 sm:py-36">
+          <UpayLinkCard upi_data={upi_data} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,41 @@
 import type { NextPage } from 'next'
 import UpayCard from '../components/UpayCard'
-
+import Head from 'next/head'
 const Home: NextPage = () => {
   return (
-    <div className="h-screen w-full content-center overflow-y-clip bg-gradient-to-br from-teal-600 to-blue-600">
-      <div className="py-14 px-2 sm:py-36">
-        <UpayCard />
+    <>
+      <Head>
+        <title>UPay</title>
+        <meta name="title" content="UPay" />
+        <meta
+          name="description"
+          content="Generate shareable UPI payment links"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://upay.cf/" />
+        <meta property="og:title" content="UPay" />
+        <meta
+          property="og:description"
+          content="Generate shareable UPI payment links"
+        />
+        <meta property="og:image" content="https://upay.cf/card.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://upay.cf/" />
+        <meta property="twitter:title" content="UPay" />
+        <meta
+          property="twitter:description"
+          content="Generate shareable UPI payment links"
+        />
+        <meta property="twitter:image" content="https://upay.cf/card.png" />
+      </Head>
+      <div className="h-screen w-full content-center overflow-y-clip bg-gradient-to-br from-teal-600 to-blue-600">
+        <div className="py-14 px-2 sm:py-36">
+          <UpayCard />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
