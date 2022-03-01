66191ad5ebba61a14e89fc4e6a5eb474f3072ae2
Amal Shaji <amalshajid@gmail.com>
2022-03-01 19:13:22 +0530

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,13 @@ function Footer() {
   return (
     <footer>
       <div className="fixed bottom-0 mx-auto mb-8 w-full text-center text-sm font-medium text-slate-50">
-        Created by{' '}
         <a
-          href="https://github.com/amalshaji"
+          href="https://github.com/amalshaji/upay"
           target="_blank"
           rel="noreferrer"
           className="text-slate-50 underline"
         >
-          Amal
+          Source
         </a>
       </div>
     </footer>
